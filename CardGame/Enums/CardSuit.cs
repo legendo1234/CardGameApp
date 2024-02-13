dbecfc4e37e06310e85f2c62f6e72d6d845b519a
Matej Weber <matej.weber@gmail.com>
2024-02-13 22:33:10 +0100

    Added stuff in main so you can run it and try it

--- CardGame/Enums/CardSuit.cs
+++ CardGame/Enums/CardSuit.cs
@@ -5,4 +5,3 @@
         Diamonds,
         Spades
     }
-}
--- CardGame/Program.cs
+++ CardGame/Program.cs
@@ -9,10 +9,14 @@ namespace CardGame
     internal class Program
     {
         static void Main(string[] args)
-        {
-            PokerDeck deck = new PokerDeck();
+        { 
+            BlackjackDeck deck = new BlackjackDeck();
 
-            
+            var hand = deck.DealCards();
+            foreach (var card in hand)
+            {
+                Console.WriteLine($"{card.Value.ToString()} of {card.Suit.ToString()}");
+            }
 
 
             Console.ReadLine();
